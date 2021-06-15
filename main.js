80d456e52c2c0592c719a236a372d3fdd4dc0f2f
danielpuchner-swp <daniel.puchner@softwarepark.cc>
2021-06-15 10:40:17 +0200

    adding auth

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 let icon =
     '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>'
 
+async function authorize(){
+    const isAuthorized = await miro.isAuthorized()
+
+    if (!isAuthorized) {
+        console.log('try to authorize');
+        // Ask the user to authorize the app.
+        var result=await miro.requestAuthorization();
+        console.log(result);
+
+    }
+}
+
 miro.onReady(() => {
     miro.initialize({
         extensionPoints: {
@@ -9,6 +21,8 @@ miro.onReady(() => {
                 svgIcon: icon,
                 positionPriority: 1,
                 onClick: () => {
+                    authorize();
+
                     miro.board.ui.openLeftSidebar('/my-first-miro-plugin/sidebar.html')
                 },
             },
